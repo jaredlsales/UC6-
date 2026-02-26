0e3eff2283f9ead02af35bd1929fa50b582a8045
Jared <jaredlsales@outlook.com>
2026-02-26 14:44:05 -0300

    26-02 - (Compilar com vite - Teste Hospedagem)

--- frontcrud/vite.config.js
+++ frontcrud/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //caminho que ele tem que fazer quando compilar
+  base:"/",
   plugins: [react()],
 })
